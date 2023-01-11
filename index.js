5fd6fecae9b1d949236c36a8f793e496b5350f7d
Mario Garcia <mariogarciacoa@gmail.com>
2023-01-11 07:16:04 -0400

    fix bug

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.delete("/api/persons/:id", (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3002
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
 	console.log(`Running in PORT ${PORT}`)
 })
